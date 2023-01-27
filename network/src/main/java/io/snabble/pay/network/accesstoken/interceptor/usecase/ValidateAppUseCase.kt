73f53879969ea8dee3ae0f8e0e0530173a8270df
Fabian Bender <fabian.bender@snabble.io>
2023-01-27 20:57:27 +0100

    implement ValidateAppUseCase and test

--- network/src/main/java/io/snabble/pay/network/accesstoken/interceptor/usecase/ValidateAppUseCase.kt
+++ network/src/main/java/io/snabble/pay/network/accesstoken/interceptor/usecase/ValidateAppUseCase.kt
@@ -1,8 +1,24 @@
 package io.snabble.pay.network.accesstoken.interceptor.usecase
 
-import io.snabble.pay.network.accesstoken.repository.AppCredentials
+import io.snabble.pay.network.accesstoken.repository.AppCredentialsRepository
+import io.snabble.pay.network.api.data.AppCredentials
 
 interface ValidateAppUseCase {
 
     suspend operator fun invoke(): AppCredentials?
+}
+
+class ValidateAppUseCaseImpl(
+    private val appCredentialsRepository: AppCredentialsRepository,
+    private val fetchAppCredentials: FetchAppCredentialsUseCase
+) : ValidateAppUseCase {
+
+    override suspend fun invoke(): AppCredentials? {
+        var appCredentials = appCredentialsRepository.getAppCredentials()
+        if (appCredentials != null) return appCredentials
+
+        appCredentials = fetchAppCredentials()
+
+        return appCredentials
+    }
 }
---
+++ network/src/test/java/io/snabble/pay/network/appcredentials/ValidateAppUseCaseTest.kt
@@ -0,0 +1,60 @@
+package io.snabble.pay.network.appcredentials
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.snabble.pay.network.accesstoken.interceptor.usecase.FetchAppCredentialsUseCase
+import io.snabble.pay.network.accesstoken.interceptor.usecase.ValidateAppUseCaseImpl
+import io.snabble.pay.network.accesstoken.repository.AppCredentialsRepository
+import io.snabble.pay.network.api.data.AppCredentials
+
+class ValidateAppUseCaseTest : FreeSpec({
+
+    val appCredentialsRepository: AppCredentialsRepository = mockk(relaxed = true)
+    val fetchAppCredentials: FetchAppCredentialsUseCase = mockk(relaxed = true)
+
+    val sut = ValidateAppUseCaseImpl(appCredentialsRepository, fetchAppCredentials)
+
+    beforeEach {
+        clearAllMocks()
+    }
+
+
+    "A validation succeeds if: " - {
+
+        "it can load the credentials locally" {
+            val expected = AppCredentials(
+                "", "", ""
+            )
+            coEvery { appCredentialsRepository.getAppCredentials() } returns expected
+
+            val appCredentials = sut()
+
+            appCredentials shouldBe expected
+
+        }
+        "local load fails but fetching is successfully" {
+            val expected = AppCredentials(
+                "", "", ""
+            )
+            coEvery { appCredentialsRepository.getAppCredentials() } returns null
+            coEvery { fetchAppCredentials.invoke() } returns expected
+            val appCredentials = sut()
+
+            appCredentials shouldBe expected
+
+        }
+    }
+
+    "A validation fails if:" -{
+        "local load and fetching fails " {
+            coEvery { appCredentialsRepository.getAppCredentials() } returns null
+            coEvery { fetchAppCredentials.invoke() } returns null
+            val appCredentials = sut()
+
+            appCredentials shouldBe null
+        }
+    }
+})

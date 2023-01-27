e7b6a51aaf068d63dd4343682e4fe2850cee4128
Fabian Bender <fabian.bender@snabble.io>
2023-01-27 21:10:17 +0100

    implement FetchAppCredentialsUseCase

--- network/src/main/java/io/snabble/pay/network/accesstoken/interceptor/usecase/FetchAppCredentialsUseCase.kt
+++ network/src/main/java/io/snabble/pay/network/accesstoken/interceptor/usecase/FetchAppCredentialsUseCase.kt
@@ -1,6 +1,9 @@
 package io.snabble.pay.network.accesstoken.interceptor.usecase
 
 import io.snabble.pay.network.api.data.AppCredentials
+import io.snabble.pay.network.api.interfaces.GetAppCredentials
+import retrofit2.Retrofit
+import retrofit2.create
 
 interface FetchAppCredentialsUseCase {
 
@@ -10,6 +13,11 @@ interface FetchAppCredentialsUseCase {
 class FetchAppCredentialsUseCaseImpl : FetchAppCredentialsUseCase {
 
     override suspend fun invoke(): AppCredentials? {
-        TODO("Not yet implemented")
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.snabble.io/app/")
+            .build()
+        val registerEndpoint = retrofit.create(GetAppCredentials::class.java)
+        val response =  registerEndpoint.getAppCredentials().execute()
+        return response.body()?.credentials
     }
 }

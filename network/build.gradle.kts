84efc7fa8f9133979dd79977f08eeeb392c72220
Fabian Bender <fabian.bender@snabble.io>
2023-01-27 09:27:05 +0100

    add dependencies for retrofit

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    kotlin("plugin.serialization") version "1.7.20"
+
 }
 
 java {
@@ -13,6 +15,9 @@ dependencies {
     implementation(libs.kotlinx.coroutinesCore)
 
     implementation(libs.bundles.testing)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
 }
 

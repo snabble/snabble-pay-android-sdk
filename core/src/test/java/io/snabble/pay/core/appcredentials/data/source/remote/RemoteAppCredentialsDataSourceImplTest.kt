a265522cc66401945fb4c26b4ea21c36d29e45d2
Fabian Bender <fabian.bender@snabble.io>
2023-02-02 21:40:44 +0100

    add remote app credentials data source impl test

---
+++ core/src/test/java/io/snabble/pay/core/appcredentials/data/source/remote/RemoteAppCredentialsDataSourceImplTest.kt
@@ -0,0 +1,43 @@
+package io.snabble.pay.core.appcredentials.data.source.remote
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.snabble.pay.network.service.register.AppRegistrationService
+import io.snabble.pay.network.service.register.dto.AppCredentialsDto
+import retrofit2.Response
+
+class RemoteAppCredentialsDataSourceImplTest : FreeSpec({
+
+    val appRegistrationService: AppRegistrationService = mockk(relaxed = true)
+    val sut = RemoteAppCredentialsDataSourceImpl(appRegistrationService)
+    beforeEach {
+        clearAllMocks()
+    }
+
+    "The remote data source" - {
+        "return app credentials on success" {
+            coEvery {
+                appRegistrationService.getAppCredentials().execute()
+            } returns Response.success(
+                AppCredentialsDto("test", "secret")
+            )
+            val appCredentials = sut.fetchAppCredentials()
+
+            appCredentials?.id?.id.shouldBe("test")
+            appCredentials?.secret?.secret.shouldBe("secret")
+        }
+        "return null on error" {
+            coEvery {
+                appRegistrationService.getAppCredentials().execute()
+            } returns Response.success(
+                null
+            )
+            val appCredentials = sut.fetchAppCredentials()
+
+            appCredentials.shouldBe(null)
+        }
+    }
+})

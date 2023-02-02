a1a2eeb039d2b8edf9ba54b757b3b2b0f1c6028a
Fabian Bender <fabian.bender@snabble.io>
2023-02-02 21:39:47 +0100

    add mapping function for AppCredentialsDto

--- core/src/main/java/io/snabble/pay/core/appcredentials/domain/model/AppCredentials.kt
+++ core/src/main/java/io/snabble/pay/core/appcredentials/domain/model/AppCredentials.kt
@@ -1,9 +1,17 @@
 package io.snabble.pay.core.appcredentials.domain.model
 
+import io.snabble.pay.network.service.register.dto.AppCredentialsDto
+
 data class AppCredentials(
     val id: AppIdentifier,
     val secret: AppSecret,
 )
+
+fun AppCredentialsDto.toAppCredentials(): AppCredentials =
+    AppCredentials(
+        AppIdentifier(id = appId),
+        AppSecret(secret = appSecret)
+    )
 
 @JvmInline
 value class AppIdentifier(val id: String)

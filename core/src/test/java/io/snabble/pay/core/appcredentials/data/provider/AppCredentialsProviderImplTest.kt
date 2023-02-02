75fe361a9d7eb3a68f7838fb3a34d516b4a824c6
Fabian Bender <fabian.bender@snabble.io>
2023-02-02 22:06:51 +0100

    add provider test

---
+++ core/src/test/java/io/snabble/pay/core/appcredentials/data/provider/AppCredentialsProviderImplTest.kt
@@ -0,0 +1,39 @@
+package io.snabble.pay.core.appcredentials.data.provider
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.snabble.pay.core.appcredentials.domain.model.AppCredentials
+import io.snabble.pay.core.appcredentials.domain.repository.AppCredentialsRepository
+
+class AppCredentialsProviderImplTest : FreeSpec({
+
+    val appCredentialsRepository: AppCredentialsRepository = mockk(relaxed = true)
+    val sut = AppCredentialsProviderImpl(appCredentialsRepository)
+
+    val expectedAppCredentials: AppCredentials = mockk(relaxed = true)
+
+    beforeEach {
+        clearAllMocks()
+    }
+
+    "The app credentials provider returns" - {
+        "app credentials as dto if available " {
+            coEvery { appCredentialsRepository.getAppCredentials() } returns expectedAppCredentials
+
+            val appCredentialsDto = sut.getAppCredentials()
+
+            appCredentialsDto?.appId.shouldBe(expectedAppCredentials.id.id)
+        }
+        "null if not available" {
+            coEvery { appCredentialsRepository.getAppCredentials() } returns null
+
+            val appCredentialsDto = sut.getAppCredentials()
+
+            appCredentialsDto?.shouldBe(null)
+        }
+    }
+
+})

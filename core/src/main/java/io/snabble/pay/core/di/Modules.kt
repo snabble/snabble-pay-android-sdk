118a687afdcb145c4ed18e6ba017f70e0709f2cb
Fabian Bender <fabian.bender@snabble.io>
2023-02-04 22:13:28 +0100

    add app credentials di and update modules

--- core/src/main/java/io/snabble/pay/core/di/Modules.kt
+++ core/src/main/java/io/snabble/pay/core/di/Modules.kt
@@ -1,8 +1,10 @@
 package io.snabble.pay.core.di
 
+import io.snabble.pay.core.appcredentials.di.appCredentials
 import io.snabble.pay.core.di.modules.services
 import org.koin.core.module.Module
 
 internal val koinModules: List<Module> = listOf(
     services,
+    appCredentials
 )

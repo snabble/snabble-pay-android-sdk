472acdc566327edd72d5fccd1cfc7293befc9e3b
Christian Maier <christian.maier@snabble.io>
2023-02-08 15:10:57 +0100

    Update the docs and add links for the account service

--- network/src/main/java/io/snabble/pay/network/service/account/AccountService.kt
+++ network/src/main/java/io/snabble/pay/network/service/account/AccountService.kt
@@ -11,17 +11,40 @@ import retrofit2.http.Query
 interface AccountService {
 
     /**
-     * [API Endpoint Documentation](https://snabble.atlassian.net/wiki/spaces/PAYMENT/pages/131301398/Backend+Requirements#Account-Endpoints)
+     * Creates a new account check object with a link to an account check service provider
+     * to add an account to Snabble Pay
+     *
+     * [Docs to add an account](https://snabble.atlassian.net/wiki/spaces/PAYMENT/pages/131301398/Backend+Requirements#Add-a-new-Account)
      */
     @GET("/apps/accounts/check")
     suspend fun getAccountCheck(@Query("appUri") appUri: String): ApiResponse<AccountCheckDto>
 
+    /**
+     * Returns a list of all accounts associate with this Snabble Pay instance.
+     * The list is empty if there are no accounts.
+     *
+     * [Docs to get all accounts](https://snabble.atlassian.net/wiki/spaces/PAYMENT/pages/131301398/Backend+Requirements#Get-a-specific-account)
+     */
     @GET("/apps/accounts")
     suspend fun getAccounts(): ApiResponse<List<AccountDto>>
 
+    /**
+     * With an account id the specific account is fetched.
+     * If the user has insufficient rights to access the specified account or the account does not
+     * exist a HTTP 404 Not Found is returned.
+     *
+     * [Docs to get a specifig account](https://snabble.atlassian.net/wiki/spaces/PAYMENT/pages/131301398/Backend+Requirements#Get-a-specific-account)
+     */
     @GET("/apps/accounts/{id}")
     suspend fun getAccount(@Path("id") id: String): ApiResponse<AccountDto>
 
+    /**
+     * An account can be removed with the account's id.
+     * If the user has insufficient rights to delete the specified account or the account does not
+     * exist a HTTP 404 Not Found is returned.
+     *
+     * [Docs to delete an account](https://snabble.atlassian.net/wiki/spaces/PAYMENT/pages/131301398/Backend+Requirements#Delete-an-account)
+     */
     @DELETE("/apps/accounts/{id}")
     suspend fun removeAccount(@Path("id") id: String): ApiResponse<AccountDto>
 }

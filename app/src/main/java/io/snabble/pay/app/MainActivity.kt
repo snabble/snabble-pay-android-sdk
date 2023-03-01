aa2141b208a902d27fbacf698e6e84f458acd34d
Fabian Bender <fabian.bender@snabble.io>
2023-03-01 13:09:05 +0100

    delete obsolete image

--- app/src/main/java/io/snabble/pay/app/MainActivity.kt
+++ app/src/main/java/io/snabble/pay/app/MainActivity.kt
@@ -6,15 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import com.google.zxing.client.android.BuildConfig
-import io.snabble.pay.app.ui.accountcard.AccountCardPager
 import io.snabble.pay.app.ui.homescreen.HomeScreen
 import io.snabble.pay.app.ui.theme.SnabblePayTheme
 import io.snabble.pay.core.dsl.snabblePay

a514edac03f0b934ab14f3685c4ce2ef932b8802
Christian Maier <christian.maier@snabble.io>
2023-03-03 13:58:44 +0100

    Cleanup SnabblePay implementation

--- library/core/src/main/java/io/snabble/pay/core/SnabblePay.kt
+++ library/core/src/main/java/io/snabble/pay/core/SnabblePay.kt
@@ -1,22 +1,9 @@
 package io.snabble.pay.core
 
-import io.snabble.pay.account.domain.model.Account
-import io.snabble.pay.account.domain.model.AccountCheck
 import io.snabble.pay.core.features.AccountSupport
 import io.snabble.pay.core.features.MandateSupport
 
-interface SnabblePay : MandateSupport {
-
-    suspend fun addNewAccount(
-        appUri: String,
-        city: String,
-        twoLetterIsoCountryCode: String,
-    ): Result<AccountCheck>
-
-    suspend fun getAccount(id: String): Result<Account>
-
-    suspend fun getAccounts(): Result<List<Account>>
-}
+interface SnabblePay : AccountSupport, MandateSupport
 
 class SnabblePayImpl internal constructor(
     accountSupport: AccountSupport,

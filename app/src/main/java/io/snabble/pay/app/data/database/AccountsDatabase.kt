f2fdc50668f4509a62c894b9c169ee13ada41f08
Fabian Bender <fabian.bender@snabble.io>
2023-03-07 11:30:21 +0100

    update to renamed type converter

--- app/src/main/java/io/snabble/pay/app/data/database/AccountsDatabase.kt
+++ app/src/main/java/io/snabble/pay/app/data/database/AccountsDatabase.kt
@@ -5,10 +5,10 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import io.snabble.pay.app.data.dao.AccountDao
 import io.snabble.pay.app.data.entity.AccountCard
-import io.snabble.pay.app.data.entity.LocalDateTimeConverter
+import io.snabble.pay.app.data.entity.ZonedDateTimeConverter
 
 @Database(entities = [AccountCard::class], version = 1, exportSchema = false)
-@TypeConverters(LocalDateTimeConverter::class)
+@TypeConverters(ZonedDateTimeConverter::class)
 abstract class AccountsDatabase : RoomDatabase() {
 
     abstract fun accountDao(): AccountDao

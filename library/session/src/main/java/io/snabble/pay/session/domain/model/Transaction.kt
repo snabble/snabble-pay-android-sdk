e2eaa6901182e7e60db593ef0cf1e072f0b829e2
Christian Maier <cmaier@users.noreply.github.com>
2023-03-16 12:26:00 +0100

    Change transactions amount type from `String` to `Int` (#12)
    Change transactions amount type from `String` to `Int` and rename the
    _currency_ to _currencyCode_.

--- library/session/src/main/java/io/snabble/pay/session/domain/model/Transaction.kt
+++ library/session/src/main/java/io/snabble/pay/session/domain/model/Transaction.kt
@@ -1,8 +1,8 @@
 package io.snabble.pay.session.domain.model
 
 data class Transaction(
-    val amount: String,
-    val currency: String,
+    val amount: Int,
+    val currencyCode: String,
     val id: String,
     val state: TransactionState,
 )

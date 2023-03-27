27f5f4ac8ab4a68dec7f6c8443206b0fdf97c0a7
Christian Maier <christian.maier@snabble.io>
2023-03-27 09:31:59 +0200

    Add missing @Body annotation for create customer info

--- library/customerinfo/src/main/java/io/snabble/pay/customerinfo/data/service/CustomerInfoService.kt
+++ library/customerinfo/src/main/java/io/snabble/pay/customerinfo/data/service/CustomerInfoService.kt
@@ -2,6 +2,7 @@ package io.snabble.pay.customerinfo.data.service
 
 import io.snabble.pay.api.retrofit.ApiResponse
 import io.snabble.pay.customerinfo.data.dto.CustomerInfoDto
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PUT
@@ -9,7 +10,9 @@ import retrofit2.http.PUT
 internal interface CustomerInfoService {
 
     @PUT("/apps/customer")
-    suspend fun createCustomerInfo(customerInfo: CustomerInfoDto): ApiResponse<CustomerInfoDto>
+    suspend fun createCustomerInfo(
+        @Body customerInfo: CustomerInfoDto,
+    ): ApiResponse<CustomerInfoDto>
 
     @GET("/apps/customer")
     suspend fun getCustomerInfo(): ApiResponse<CustomerInfoDto>

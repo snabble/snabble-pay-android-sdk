cb1f6f0a39fb339f531aff2e06597e363084e457
Christian Maier <christian.maier@snabble.io>
2023-03-29 15:46:40 +0200

    Add docs for the CustomerInfo model

--- library/customerinfo/src/main/java/io/snabble/pay/customerinfo/domain/model/CustomerInfo.kt
+++ library/customerinfo/src/main/java/io/snabble/pay/customerinfo/domain/model/CustomerInfo.kt
@@ -1,5 +1,11 @@
 package io.snabble.pay.customerinfo.domain.model
 
+/**
+ * CustomerInfo that is associated to a customer's transaction.
+ *
+ * @property id An optional ID
+ * @property loyaltyId An optional LoyaltyID
+ */
 data class CustomerInfo(
     val id: String? = null,
     val loyaltyId: String? = null,

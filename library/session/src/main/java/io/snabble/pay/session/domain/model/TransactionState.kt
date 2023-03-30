ba29e051bf001c9cd112d9908cc06c472ecc5785
Fabian Bender <fabian.bender@snabble.io>
2023-03-30 13:13:08 +0200

    update TransactionState.kt doc

--- library/session/src/main/java/io/snabble/pay/session/domain/model/TransactionState.kt
+++ library/session/src/main/java/io/snabble/pay/session/domain/model/TransactionState.kt
@@ -1,13 +1,40 @@
 package io.snabble.pay.session.domain.model
 
-/** Enum class representing the state of a [Transaction].*/
+/** Represents the state of a [Transaction].*/
 enum class TransactionState {
 
+    /**
+     * Final state if a transaction has been aborted by the customer.
+     */
     ABORTED,
+
+    /**
+     * Final state if a transaction ended in an error state.
+     */
     ERRORED,
+
+    /**
+     * Final state if a transaction has failed.
+     */
     FAILED,
+
+    /**
+     * State if a transaction is still in process.
+     */
     PENDING,
+
+    /**
+     * Pre state if a transaction needs to be authorized.
+     */
     PREAUTHORIZED,
+
+    /**
+     * Final state if a transaction authorization failed.
+     */
     PREAUTH_FAILED,
+
+    /**
+     * Final state if a transaction has been successfully finished by the customer.
+     */
     SUCCESSFUL,
 }

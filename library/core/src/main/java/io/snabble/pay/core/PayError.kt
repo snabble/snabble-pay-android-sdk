368abe4320c03d66eb7023024fb94fbc6819a334
Fabian Bender <fabian.bender@snabble.io>
2023-03-30 15:17:21 +0200

    update Result.kt and PayError.kt doc

--- library/core/src/main/java/io/snabble/pay/core/PayError.kt
+++ library/core/src/main/java/io/snabble/pay/core/PayError.kt
@@ -1,28 +1,105 @@
 package io.snabble.pay.core
 
+/**
+ * Represents the snabble pay error.
+ *
+ * @property reason Server error with snabble endpoints
+ * @property message Describes the reason or contains the exception message
+ * @property exception Exception returned by the request
+ */
 data class PayError(
     val reason: Reason,
     val message: String? = null,
     val exception: Throwable? = null,
 )
 
+/**
+ * Represents an Error occurring on the pay server.
+ */
 enum class Reason {
 
+    /**
+     * Account for the given id not found
+     */
     ACCOUNT_NOT_FOUND,
+
+    /**
+     * Customer for the given id not found
+     */
     CUSTOMER_NOT_FOUND,
+
+    /**
+     * Testing/Debug purpose
+     */
     LOCAL_MANDATE,
+
+    /**
+     * Internal server error occurred
+     */
     INTERNAL_ERROR,
+
+    /**
+     * Update token couldn"t be done due to transaction already startet
+     */
     INVALID_SESSION_STATE,
+
+    /**
+     * Update transaction failed since transaction is PREAUTHORIZED_SUCCESSFUL state
+     */
     INVALID_TRANSACTION_STATE,
+
+    /**
+     * Session start failed due to missing accepted mandate
+     */
     MANDATE_NOT_ACCEPTED,
+
+    /**
+     * Delete session has failed due to running transaction
+     */
     SESSION_HAS_TRANSACTION,
+
+    /**
+     * Session for the given id not found
+     */
     SESSION_NOT_FOUND,
+
+    /**
+     * Session token is expired, e.g. INVALID
+     */
     SESSION_TOKEN_EXPIRED,
+
+    /**
+     * Token for the given id not found
+     */
     TOKEN_NOT_FOUND,
+
+    /**
+     * A transaction is already running for the current session
+     */
     TRANSACTION_ALREADY_STARTED,
+
+    /**
+     * Transaction for the given id not found
+     */
     TRANSACTION_NOT_FOUND,
+
+    /**
+     * Access token is expired or invalid
+     */
     UNAUTHORIZED,
+
+    /**
+     * Unknown error
+     */
     UNKNOWN,
+
+    /**
+     * User for the given id not found
+     */
     USER_NOT_FOUND,
+
+    /**
+     * Missing or invalid informatione
+     */
     VALIDATION_ERROR,
 }
--- library/core/src/main/java/io/snabble/pay/core/util/Result.kt
+++ library/core/src/main/java/io/snabble/pay/core/util/Result.kt
@@ -2,6 +2,17 @@ package io.snabble.pay.core.util
 
 import io.snabble.pay.core.PayError
 
+/**
+ * Result wrapper for return value.
+ *
+ * Returns the requested object or `null` on [Success] or an exception, with the responding [PayError] on [Failure].
+ * The return value can be accessed via two ways:
+ * type check: [Success] or [Failure]
+ * properties [isSuccess] or [isFailure], receive the object via the corresponding functions [getOrNull] and [exceptionOrNull]
+ *
+ * @property isSuccess True if the request for the result has succeeded
+ * @property isFailure True if the request for the result has failed
+ */
 sealed class Result<out T> {
 
     val isSuccess: Boolean get() = this is Success
@@ -28,7 +39,12 @@ sealed class Result<out T> {
             Failure(exception, error)
     }
 }
-
+/**
+ * Represents a result that succeeded, containing the requested object
+ */
 data class Success<T>(val value: T) : Result<T>()
 
+/**
+ * Represents a result that failed, containing the exception, and the [PayError]
+ */
 data class Failure(val exception: Throwable, val error: PayError) : Result<Nothing>()

9a295c141af13ed6da4f36bf1de6ceaaad749d37
Fabian Bender <fabian.bender@snabble.io>
2023-04-12 11:57:16 +0200

    delete obsolete module

--- library/internal/src/main/java/io/snabble/pay/internal/di/SharedModule.kt
+++
@@ -1,9 +0,0 @@
-package io.snabble.pay.internal.di
-
-import io.snabble.pay.internal.account.data.mapper.MandateStateMapper
-import org.koin.dsl.module
-
-val sharedModule = module {
-    single { io.snabble.pay.internal.account.data.mapper.AccountMapper(mandateStateMapper = get()) }
-    single { MandateStateMapper() }
-}

6a0172eee8f88a6cdafb9debd2a8513dc5cc8d79
Christian Maier <cmaier@users.noreply.github.com>
2023-06-30 13:38:10 +0200

    Fix status bar background and icons color (#31)

--- app/src/main/java/io/snabble/pay/app/ui/theme/Theme.kt
+++ app/src/main/java/io/snabble/pay/app/ui/theme/Theme.kt
@@ -1,18 +1,14 @@
 package io.snabble.pay.app.ui.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val DarkColorScheme = darkColorScheme(
+private val darkColorScheme = darkColorScheme(
     primary = Color.White,
     secondary = Color(0x7f0077BB),
     background = Color(0xFF1D1A22),
@@ -20,7 +16,7 @@ private val DarkColorScheme = darkColorScheme(
     onSecondary = Color(0xFF4BB4EF)
 )
 
-private val LightColorScheme = lightColorScheme(
+private val lightColorScheme = lightColorScheme(
     primary = Color.White,
     secondary = Color(0x7f0077BB),
     background = Color(0xFFFBFBFF),
@@ -34,17 +30,12 @@ fun SnabblePayTheme(
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkColorScheme
+        else -> lightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
+
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(color = colorScheme.background)
 
     MaterialTheme(
         colorScheme = colorScheme,
